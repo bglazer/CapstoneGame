34d31ff1fda182d966beeaae07fa99d6d125cd38
Bryan Glazer <bglazer@uark.edu>
2012-06-29 16:05:59 -0500

    Added filename extractor and include guards

--- include/common.h
+++ include/common.h
@@ -1,9 +1,24 @@
 #include <boost/flyweight.hpp>
 #include <boost/flyweight/key_value.hpp>
+#include "FwImage.h"
 
 /* Header file that contains typedefs and forward declarations common to most files */
 
+#ifndef COMMON
+#define COMMON 
+
+struct img_filename_extractor
+{
+    const std::string& operator()(const FwImage& x)const
+    {
+        return x.getName();
+    }
+};
+
 typedef boost::flyweight< boost::flyweights::key_value<
                             std::string,
-                            sf::Image>
+                            sf::Image,
+                            img_filename_extractor>
                         > img_flyweight;
+
+#endif                        
