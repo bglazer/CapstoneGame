0a682b54b5907d80e3eddc5e90d871adea7c1a97
Bryan Glazer <bglazer@uark.edu>
2012-07-02 15:21:56 -0500

    Added a bunch of physics stuff. Needs to be refactored into appropriate classes, ok for testing now.

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,6 @@
 #include "../include/SFML/Window/Window.hpp"
 #include "../include/SFML/Graphics.hpp"
+#include "../include/Box2D/Box2D.h"
 #include "../include/Player.h"
 #include "../include/Block.h"
 #include "../include/StaticBlock.h"
@@ -20,6 +21,10 @@ sf::Clock gameClock;
 ImageLoader* img_loader;
 std::vector<sf::Image*>* imgs;
 std::vector<sf::Sprite*>* sprites;
+b2Vec2* gravity;
+b2World* world;
+b2BodyDef *groundBodyDef, *dynamicBodyDef;
+b2Body *groundBody, *dynamicBody;
 
 bool quit = false;
 
@@ -30,6 +35,9 @@ void cleanup();
 
 int main( int argc, char* argv[] )
 {
+	B2_NOT_USED(argc);
+	B2_NOT_USED(argv);
+
     if(initialize())
     {
         while(quit == false)
@@ -51,6 +59,23 @@ bool initialize()
     imgs = new std::vector<sf::Image*>();
     sprites = new std::vector<sf::Sprite*>();
 
+    gravity = new b2Vec2(0.0f, -10.0f);
+    world = new b2World(*gravity);
+    groundBodyDef = new b2BodyDef();
+    groundBodyDef->position.Set(0.0f, -10.0f);
+    groundBody = world->CreateBody(groundBodyDef);
+    dynamicBodyDef = new b2BodyDef();
+    dynamicBodyDef->type = b2_dynamicBody;
+    dynamicBodyDef->position.Set(0.0f, 4.0f);
+    dynamicBody = world->CreateBody(dynamicBodyDef);
+    b2PolygonShape dynamicBox;
+    dynamicBox.SetAsBox(1.0f, 1.0f);
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &dynamicBox;
+    fixtureDef.density = 1.0f;
+    fixtureDef.friction = 0.3f;
+    dynamicBody->CreateFixture(&fixtureDef);
+
     img_flyweight* block_fw = img_loader->load_image( "./resources/block.png" ); 
     FwImage block_fwimg = block_fw->get();
     sf::Image* block_img = &block_fwimg.getImage();
@@ -58,6 +83,8 @@ bool initialize()
     imgs->push_back(block_img);
 
     sprites->push_back(new sf::Sprite(*block_img));
+
+    return 1;
 }
 
 void update()
@@ -75,6 +102,11 @@ void update()
     float ElapsedTime = gameClock.GetElapsedTime();
     gameClock.Reset();
    
+    world->Step(1.0f/FRAMES_PER_SECOND, 6, 2);
+
+    b2Vec2 position = dynamicBody->GetPosition();
+    float32 angle = dynamicBody->GetAngle();
+
 	while( app->GetEvent(event) )
 	{
         if( event.Type == sf::Event::Closed )
@@ -82,7 +114,7 @@ void update()
 
         if( (event.Type == sf::Event::KeyPressed) && (event.Key.Code == sf::Key::Escape))
             quit = true;
-
+/*
         if( leftKeyPress )
             sprites->at(0)->Move(-10,0);
         if( rightKeyPress )
@@ -91,8 +123,8 @@ void update()
             sprites->at(0)->Move(0,-10);
         if( downKeyPress )
             sprites->at(0)->Move(0,10);
+*/
     }
-    
 
     //Player->update
 }
@@ -114,6 +146,8 @@ void draw()
 void cleanup()
 {
     delete img_loader;
+    delete world;
+    delete gravity;
     delete imgs;
     delete sprites;
     delete app;
