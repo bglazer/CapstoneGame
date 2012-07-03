273789c220ac1921c85a08683bc7cad109e5d745
Bryan Glazer <bglazer@uark.edu>
2012-07-03 14:14:38 -0500

    Added necessary declarations to Block.h to build class

--- include/Block.h
+++ include/Block.h
@@ -6,6 +6,9 @@
  */
 
 #include "PhysicalObject.h"
+#include "SFML/Graphics.hpp"
+#include "ImageLoader.h"
+#include "Box2D.h"
 
 #ifndef BLOCK_H
 #define	BLOCK_H
@@ -13,10 +16,17 @@
 class Block : public PhysicalObject
 {
         public:
-            Block();
+            Block(b2Vec2* size, b2Vec2* position, float32 angle, b2World* world, sf::RenderWindow* app);
+            Block(b2Vec2* position, float32 angle, b2World* world, sf::RenderWindow* app, ImageLoader &img_loader, std::string filename);
             void render();
             void update();
         private:
+            b2BodyDef *blockBodyDef;
+            b2Body *blockBody;
+            sf::Sprite *sprite;
+            bool using_image;
+            const FwImage* block_fwimg;
+            const sf::Image* block_img;
 };
 
 #endif 
