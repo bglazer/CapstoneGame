cc973e394ce6e83fb6cf4b6920b04ec230d593c8
Bryan Glazer <bglazer@uark.edu>
2012-07-03 23:08:24 -0500

    Implemented StaticBlock

--- src/StaticBlock.cc
+++ src/StaticBlock.cc
@@ -1,45 +1,38 @@
 #include "../include/StaticBlock.h"
-/*
-#include "../include/SDL/SDL.h"
-#include "../include/SDL/SDL_image.h"
-#include "../include/chipmunk/chipmunk.h"
-*/
-#include "../include/ImageLoader.h"
 
-//Need additional args for constructor (mass, width height etc)?
-StaticBlock::StaticBlock(/*cpSpace* space, SDL_Surface* screen, cpVect pos, int size*/)
-{
-/*
-    body = cpSpaceGetStaticBody(space);
-    shape = cpBoxShapeNew(body, 40, 40);
-//    cpBodyAddShape(body, shape);
-    this->screen = *screen;
-    
-    //cpSpaceAddBody(space, body);
-    
-    cpBodySetPos(body, pos);
-    
-    ImageLoader* il = new ImageLoader();
-    
-    img = il->load_image("../resources/block.png");
-    
-    x = pos.x;
-    y = pos.y;
-*/
+StaticBlock::StaticBlock(b2Vec2* position, float32 angle, b2World* world, sf::RenderWindow* app, ImageLoader *img_loader, std::string filename)
+{   
+    this->app = app;
+
+    img_flyweight* block_fw = img_loader->load_image( filename ); 
+    fwimg = &block_fw->get();
+    img = &fwimg->getImage();
+
+    b2Vec2 size(img->GetWidth(), img->GetHeight());
+
+    //Ground Body Def
+    staticBodyDef = new b2BodyDef();
+    staticBodyDef->position.Set(position->x, position->y);
+    staticBodyDef->angle = angle;
+    staticBody = world->CreateBody(staticBodyDef);
+
+    //Ground Polygon
+    b2PolygonShape staticBox;
+
+    staticBox.SetAsBox(size.x/PX_PER_METER/2, size.y/PX_PER_METER/2);
+
+//    std::cout << size.x/PX_PER_METER/2 << " " <<  size.y/PX_PER_METER/2 << std::endl;
+
+    staticBody->CreateFixture(&staticBox, 0.0f);
+
+    sprite = new sf::Sprite(*img);
+
+    sprite->SetPosition(position->x*PX_PER_METER, position->y*PX_PER_METER);
+    sprite->SetCenter(size.x/2, size.y/2);
 }
 
 void StaticBlock::render()
 {
-/*
-    //Holds offsets
-    SDL_Rect offset;
-
-    //Get offsets
-    offset.x = x;
-    offset.y = y;
-
-    //Blit
-    SDL_BlitSurface( img, NULL, &screen, &offset );
-*/
+    app->Draw( *sprite);
 }
 
